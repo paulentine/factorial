31550c3635f097e347cd29dc1ba00e322863fa15
Pauline Sauget <paulinevsauget@gmail.com>
2019-04-08 17:29:20 -0700

    Solved recursively

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -4,12 +4,6 @@
 def factorial(number)
   raise ArgumentError if !number
   return false if number < 0
-  # 0! & 1! is 1
   return 1 if number <= 1
-  factorial = 1
-  while number > 0
-    factorial *= number
-    number -= 1
-  end
-  return factorial
+  return number *= factorial(number - 1)
 end
