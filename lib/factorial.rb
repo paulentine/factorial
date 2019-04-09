97236e74f30965e1392ae55756d0eebb47ecded0
Pauline Sauget <paulinevsauget@gmail.com>
2019-04-08 17:24:43 -0700

    Compute factorial of a given number

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,6 +1,15 @@
 # Computes factorial of the input number and returns it
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n), linear, where n is the size of the number.
+# Space complexity: O(1), constant, takes the same amount of space.
 def factorial(number)
-  raise NotImplementedError
+  raise ArgumentError if !number
+  return false if number < 0
+  # 0! & 1! is 1
+  return 1 if number <= 1
+  factorial = 1
+  while number > 0
+    factorial *= number
+    number -= 1
+  end
+  return factorial
 end
